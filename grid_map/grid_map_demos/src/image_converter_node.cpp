8e72fcb512ae2c3a3149cfdf154633c4875ba8ed
YuxuanZhao <18218490078@163.com>
2023-05-07 11:06:04 +0800

    add gaussian curvature extrema

--- grid_map/grid_map_demos/src/image_converter_node.cpp
+++ grid_map/grid_map_demos/src/image_converter_node.cpp
@@ -11,6 +11,90 @@
 #include <fstream>
 int once = 0;
 
+void toTXT(const std::vector<cv::Mat>& src, const std::vector<std::string>& pathnames){
+    for(int i = 0; i <  pathnames.size(); i++){
+        Eigen::Matrix<float, -1, -1> target;
+        cv::cv2eigen(src[i], target);
+        std::ofstream fout(pathnames[i]);
+        if(fout.is_open()){
+            fout<<target;
+            fout.close();
+            ROS_INFO("Matrix written to file [%s] successfully.", pathnames[i].c_str());
+        } else{
+            ROS_WARN("Unable to open file [%s]", pathnames[i].c_str());
+        }
+    }
+}
+
+void find_extrema_points(cv::Mat& gauss, std::vector<cv::Point>& extrema_points, cv::Mat& extrema){
+    extrema = cv::Mat::zeros(gauss.size(), CV_32FC1);
+    // loop through each pixel in the image
+    for(int i = 1; i < gauss.rows - 1; i++){
+        for(int j = 1; j < gauss.cols - 1; j++){
+            // check if the current pixel is an extremum
+            float value = gauss.at<float>(i, j);
+            bool is_extremum = true;
+            for(int k = -1; k <= 1; k++){
+                for(int l = -1; l <= 1; l++){
+                    if(value < gauss.at<float>(i+k, j+l)){
+                        is_extremum = false;
+                        break;
+                    }
+                }
+                if(!is_extremum){
+                    break;
+                }
+            }
+            if(is_extremum){
+                extrema.at<float>(i, j) = value;
+            }
+        }
+    }
+    cv::findNonZero(extrema, extrema_points);
+}
+
+void calculate_gaussian_curvature(const cv::Mat& src, int ksize, cv::Mat& dst){
+    cv::Mat gaussBlur, dx, dy, dxx, dxy, dyy;
+    // 1. gaussian blue
+    cv::GaussianBlur(src, gaussBlur, cv::Size(5, 5), 0, 0);
+    
+    // 2. hessian
+    cv::Sobel(gaussBlur, dx, -1, 1, 0, ksize);
+    cv::Sobel(gaussBlur, dy, -1, 0, 1, ksize);
+    cv::Sobel(dx, dxx, -1, 1, 0, ksize);
+    cv::Sobel(dx, dxy, -1, 0, 1, ksize);
+    cv::Sobel(dy, dyy, -1, 0, 1, ksize);
+
+    // 3. DoH & gaussian curvature & eigen
+    dst.create(src.size(), CV_32FC1);
+    for(int i = 0; i < src.rows; i++){
+        for(int j = 0; j < src.cols; j++){
+            float fx = dx.at<float>(i, j);
+            float fy = dy.at<float>(i, j);
+            float fxx = dxx.at<float>(i, j);
+            float fxy = dxy.at<float>(i, j);
+            float fyy = dyy.at<float>(i, j);
+            // DoH
+            float doh = fxx * fyy - fxy * fxy;
+            // gaussian curvature
+            float k = doh / pow(fx * fx + fy * fy + 1e-8, 2);
+
+            // eigen value & eigen vector
+            cv::Mat hessianAtEachPoint_(2, 2, CV_32FC1);
+            hessianAtEachPoint_.at<float>(0, 0) = fxx;
+            hessianAtEachPoint_.at<float>(0, 1) = fxy;
+            hessianAtEachPoint_.at<float>(1, 0) = fxy;
+            hessianAtEachPoint_.at<float>(1, 1) = fyy;
+            cv::Mat eigenValue_, eigenVector_;
+            cv::eigen(hessianAtEachPoint_, eigenValue_, eigenVector_);
+            // float gaussCurv_ = eigenValue_.at<float>(0, 0) * eigenValue_.at<float>(1, 0);
+            // gaussianCurvature_.at<float>(i, j) = gaussCurv_;
+
+            dst.at<float>(i, j) = k;
+        }
+    }
+}
+
 void imageCallback(const sensor_msgs::ImageConstPtr& msg){
     cv_bridge::CvImagePtr cv_ptr;
     try{
@@ -20,98 +104,32 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg){
         ROS_ERROR("cv_bridge exception: %s", e.what());
         return ;
     }
-    // Eigen::Matrix<float, -1, -1> signedDistanceMatrix_;
-    // cv::cv2eigen(cv_ptr->image, signedDistanceMatrix_);
+
     cv::Mat sdfSrc_ = cv_ptr->image;
-    cv::Mat sdfBlur_, sdfDst_x_, sdfDst_y_, sdfDst_xx_, sdfDst_yy_, sdfDst_xy_;
+    cv::Mat gaussCurv_, extrema_;
+    std::vector<cv::Point> extrema_points_;
 
-    // 1. Gaussian blur
-    cv::GaussianBlur(sdfSrc_, sdfBlur_, cv::Size(5, 5), 0, 0);
+    calculate_gaussian_curvature(sdfSrc_, 3, gaussCurv_);
+    find_extrema_points(gaussCurv_, extrema_points_, extrema_);
 
-    // 2. Hessian: H_xx, H_yy, H_xy, H_yx(is equal to H_xy)
-    cv::Sobel(sdfBlur_, sdfDst_x_, -1, 1, 0, 3);
-    cv::Sobel(sdfBlur_, sdfDst_y_, -1, 0, 1, 3);
-    cv::Sobel(sdfDst_x_, sdfDst_xx_, -1, 1, 0, 3);
-    cv::Sobel(sdfDst_y_, sdfDst_yy_, -1, 0, 1, 3);
-    cv::Sobel(sdfDst_x_, sdfDst_xy_, -1, 0, 1, 3);
-
-    // 3. DoH = H_xx * H_yy - H_xy^2
-    cv::Mat sdfDoH_, sdfXXTimesYY_, sdfPowHessianXY_;
-    cv::multiply(sdfDst_xx_, sdfDst_yy_, sdfXXTimesYY_);
-    cv::pow(sdfDst_xy_, 2.0, sdfPowHessianXY_);
-    cv::subtract(sdfXXTimesYY_, sdfPowHessianXY_, sdfDoH_);
-
-    // 4. eigen value & eigen vector of Hessian
-    int sdfRows_{sdfDst_xx_.rows}, sdfCols_{sdfDst_xx_.cols};
-    cv::Mat gaussianCurvature_(sdfRows_, sdfCols_, CV_32FC1);
-    for(int i = 0; i < sdfRows_; i++){
-        for(int j = 0; j < sdfCols_; j++){
-            auto h_xx = sdfDst_xx_.at<float>(i, j);
-            auto h_yy = sdfDst_yy_.at<float>(i, j);
-            auto h_xy = sdfDst_xy_.at<float>(i, j);
-
-            cv::Mat hessianAtEachPoint_(2, 2, CV_32FC1);
-            hessianAtEachPoint_.at<float>(0, 0) = h_xx;
-            hessianAtEachPoint_.at<float>(0, 1) = h_xy;
-            hessianAtEachPoint_.at<float>(1, 0) = h_xy;
-            hessianAtEachPoint_.at<float>(1, 1) = h_yy;
-
-            cv::Mat eigenValue_, eigenVector_;
-            cv::eigen(hessianAtEachPoint_, eigenValue_, eigenVector_);
-            ROS_INFO_THROTTLE(1.0, "size of Hessian's eigen value: rows: %d, cols: %d .", eigenValue_.rows, eigenValue_.cols);
-            float gaussCurv_ = eigenValue_.at<float>(0, 0) * eigenValue_.at<float>(1, 0);
-            gaussianCurvature_.at<float>(i, j) = gaussCurv_;
-        }
-    }
-
-    // cv::Mat img_rgb;
-    // img_rgb = cv_ptr->image;
-    cv::imshow("origin", sdfSrc_);
-    cv::imshow("Gaussian Blur", sdfBlur_);
-    cv::imshow("Sobel_X", sdfDst_x_);
-    cv::imshow("Sobel_Y", sdfDst_y_);
-    cv::imshow("Sobel_XX", sdfDst_xx_);
-    cv::imshow("Sobel_YY", sdfDst_yy_);
-    cv::imshow("Sobel_XY", sdfDst_xy_);
-    cv::imshow("DoH", sdfDoH_);
-    cv::imshow("Gaussian Curvature", gaussianCurvature_);
-
+    cv::imshow("sdf message", sdfSrc_);
+    cv::imshow("Gaussian Curvature", gaussCurv_);
+    cv::waitKey(3);
 
     if(once == 0){
-        Eigen::Matrix<float, -1, -1> DoH_;
-        cv::cv2eigen(sdfDoH_, DoH_);
-
-        Eigen::Matrix<float, -1, -1> GC_;
-        cv::cv2eigen(gaussianCurvature_, GC_); 
-        
         once++;
-        std::ofstream fout("/home/yuxuanzhao/Desktop/DoH.txt");
-        fout<<DoH_;
-        fout.close();
-
-        std::ofstream fout2("/home/yuxuanzhao/Desktop/GC.txt");
-        fout2<<GC_;
-        fout2.close();
-        ROS_INFO_THROTTLE(1.0, "Write doh...");
+        std::vector<cv::Mat> data_{gaussCurv_, extrema_};
+        std::vector<std::string> filenames_{"/home/yuxuanzhao/Desktop/gaussCurv.txt", "/home/yuxuanzhao/Desktop/extrema.txt"};
+        toTXT(data_, filenames_);
     }
-
-    cv::waitKey(3);
 }
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "image_converter");
     ros::NodeHandle nh("~");
 
-    cv::namedWindow("origin", CV_WINDOW_NORMAL);
-    cv::namedWindow("Gaussian Blur", CV_WINDOW_NORMAL);
-    cv::namedWindow("Sobel_X", CV_WINDOW_NORMAL);
-    cv::namedWindow("Sobel_Y", CV_WINDOW_NORMAL);
-    cv::namedWindow("Sobel_XX", CV_WINDOW_NORMAL);
-    cv::namedWindow("Sobel_YY", CV_WINDOW_NORMAL);
-    cv::namedWindow("Sobel_XY", CV_WINDOW_NORMAL);
-    cv::namedWindow("DoH", CV_WINDOW_NORMAL);
+    cv::namedWindow("sdf message", CV_WINDOW_NORMAL);
     cv::namedWindow("Gaussian Curvature", CV_WINDOW_NORMAL);
-
     cv::startWindowThread();
 
     image_transport::ImageTransport it(nh);
